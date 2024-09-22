3782be4b78ec05f702263ce183250a7ba3eb5f34
DavidNDNS3001 <ndns.david@gmail.com>
2024-09-21 21:42:19 -0500

    RegistriO Usuarios

--- SistemaInventario.Utilidades/DS.cs
+++ SistemaInventario.Utilidades/DS.cs
@@ -12,6 +12,9 @@ namespace SistemaInventario.Utilidades
         public const string Exitosa = "Exitosa";
         public const string Error = "Error";
         public const string ImagenRuta = @"\imagenes\producto\";
+        public const string Role_Admin = "Admin";
+        public const string Role_Cliente = "Cliente";
+        public const string Role_Inventario = "Inventario";
 
     }
 }
